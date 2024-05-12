f3c46cd9f2265bb3e061c890c0550720d34caffd
Anh Pham <demonxranger8@gmail.com>
2024-05-12 00:35:55 -0400

    Updates

---
+++ app/immutable/entry/start.BX8pz58M.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.7UbPQVPZ.js";export{t as start};
---
+++ app/immutable/nodes/2.Cpb5OgdQ.js
@@ -0,0 +1,59 @@
+import{s as V,n as z,o as q,d as J}from"../chunks/scheduler.Cec-JE-B.js";import{S as U,i as Y,e as r,s as v,f as D,g as l,j as x,n as k,c as A,d as c,k as F,o as h,a as _,l as a,m as Z}from"../chunks/index.CpcIDqA5.js";const K=p=>{let s="";for(const n of p){const t=n.innerHTML;s+=t}return Math.round(s.trim().split(" ").length/225)};function Q(p){let s,n,t,I="Why we should embrace AI in education",b,o,i,W=`By
+				<a href="https://github.com/anhatthezoo" target="_blank" class="underline underline-offset-4">Anh Pham</a>`,E,T,H,w,C,N,M,u,m,O=`<h1 class="body-text"><span class="font-bold italic">Note:</span>
+			This is an opinion piece.</h1> <p class="body-text">The rise of powerful AI language models like ChatGPT has revolutionized how we communicate,
+			learn, and work. Ever since ChatGPT came out in November 2022, GPT and similar models have
+			been everywhere and started an LLM arms race. With just a simple prompt, these AI tools can
+			generate human-like text on virtually any topic. Major tech companies are rapidly
+			incorporating and building their own generative AI into their mainstream software. They’ve
+			already been integrated into Google, Grammarly, Notion, Microsoft Office, and even YouTube. As
+			AI becomes more and more ubiquitous across the most commonly used tools, students should be
+			able to know how to use them effectively and responsibly from an early age.</p> <p class="body-text">Without a doubt, generative AI provides an incredible opportunity for students to be more
+			productive and improve their skills. Instead of staring at a blank page, a student can prompt
+			a chatbot to instantly generate a rough draft of what they want to write. If they’re having
+			problems understanding a concept, they can just ask for a simplified explanation. An assigned
+			reading is too long? It could be summarized in just a few keystrokes. Beyond saving time, AI
+			can enhance the learning process by acting as a personal tutor. They can spot grammatical
+			errors, refine writing, and stimulate thinking (for example, offering multiple perspectives on
+			a topic). As of Fall 2023, half of college students have reported using AI for schoolwork
+			according to a study conducted across over 600 institutions.
+			<sup><a href="#cite1" class="citation">[1]</a></sup></p> <div class="mx-auto min-w-[700px] max-w-[700px] mb-6"><video controls="" muted="" class="w-full border"><source src="claude_summarize.mp4" type="video/mp4"/></video> <div class="mt-4"><span class="font-serif mr-1 text-gray-400">Claude 3 summarizing the Wiki page for the Maryland Terrapins.</span></div></div> <p class="body-text">With how good AI has become at producing text and even reasoning, it’s understandable that
+			there have been concerns over academic integrity. Powerful language models can write essays,
+			reports, and analyses that are well-structured and indistinguishable from something written by
+			a human expert. One of GPT-4’s selling points was that it scored in the top 10% for a bar
+			exam.
+			<sup><a href="#cite2" class="citation">[2]</a></sup>
+			Claude 3 boasted that it possesses undergraduate-level expert knowledge and can read a dense research
+			paper in less than 3 seconds.
+			<sup><a href="#cite3" class="citation">[3]</a></sup>
+			All of these services are free to use. The fear is that students may be tempted to submit an entirely
+			AI-generated output as their original work, allowing them to cheat on a massive scale.</p> <p class="body-text">However, these concerns, while valid, are likely overstated. Of course, students will always
+			find a way to cheat. A study has shown that around 60 to 70% of students have admitted to
+			cheating at some point before, and that number has stayed the same over the past few years
+			even with the new technology.
+			<sup><a href="#cite4" class="citation">[4]</a></sup>
+			Just as students could copy text from the Internet or hire someone to write essays in the past,
+			these new generative AI services are simply a new tool. Strong academic integrity policies and
+			honor codes are still effective deterrents.</p> <p class="body-text">Plus, viewing AI assistance as outright “cheating” fails to recognize the ways that these
+			tools can be used legitimately and effectively. For decades, students have gotten help from
+			tutors, friends, and family for assignments. LLMs are no different than that. When used
+			responsibly with human editing, fact-checking, and original thought, AI can be a powerful
+			writing partner rather than an unethical aid. And it looks like many students see this too. In
+			a survey conducted by the Pew Research Center, 57% of teens believe it is unacceptable to use
+			ChatGPT to write essays, while 69% of teens think it could be used for researching a new
+			topic.
+			<sup><a href="#cite5" class="citation">[5]</a></sup></p> <p class="body-text">Traditional essays, reports, and other forms of homework have to adapt to LLMs’ advanced
+			language abilities. Already, “AI detectors” marketed as anti-cheating tools have shown
+			themselves to be ineffective and even harmful, flagging formal writing such as the U.S.
+			Constitution and the Bible to be “AI generated.”
+			<sup><a href="#cite6" class="citation">[6]</a></sup></p> <div class="mx-auto min-w-[700px] max-w-[700px] mb-6"><img class="border mb-4" src="bible_ai_generated_ars_technica.jpg" alt="ZeroGPT believes that the Bible is AI generated"/> <div class="mt-4"><span class="font-serif mr-1 text-gray-400">ZeroGPT believes that the Bible is AI generated.</span> <span class="font-serif text-gray-400 text-sm italic">Ars Technica</span></div></div> <p class="body-text">Educators should shift towards more dynamic and open-ended assignments that evaluate skills
+			like reasoning, problem-solving, and creativity, abilities that current AI still lacks.
+			Additionally, developing AI literacy skills is as important as learning how to use them
+			effectively. Students should be taught about LLMs’ limitations, such as hallucinations, where
+			language models generate plausible but incorrect information, and bias.</p> <p class="body-text">One teacher in New York has incorporated AI into their curriculum by allowing students to
+			critique it.
+			<sup><a href="#cite7" class="citation">[7]</a></sup>
+			When shown a lesson plan on wearable technology created by ChatGPT, the students found that ChatGPT
+			included false information and appeared to be biased toward wearables.</p> <p class="body-text">Generative AI is rapidly transforming education and how students approach assignments. Rather
+			than banning them, educators should incorporate them into their lessons and move assignments
+			toward more open-ended ones. By embracing generative AI, the next generation can be prepared
+			to employ this new technology effectively in our changing world.</p>`,S,g,R='<h1 class="font-sans mb-8 font-semibold text-3xl mx-auto min-w-[700px] max-w-[700px]">References</h1> <ol class="mx-auto min-w-[700px] max-w-[700px] list-decimal list-inside text-md"><li class="mb-4" id="cite1"><a class="text-blue-500 underline" href="https://www.insidehighered.com/news/tech-innovation/artificial-intelligence/2023/10/31/most-students-outrunning-faculty-ai-use">Most students outrunning faculty in AI use, study finds.</a> <span>Coffey, Lauren. Inside Higher Ed. 31 October 2023.</span></li> <li class="mb-4" id="cite2"><a class="text-blue-500 underline" href="https://cdn.openai.com/papers/gpt-4.pdf">GPT-4 Technical Report.</a> <span>OpenAI. 2023.</span></li> <li class="mb-4" id="cite3"><a class="text-blue-500 underline" href="https://www.anthropic.com/news/claude-3-family">Introducing the next generation of Claude.</a> <span>Anthropic. 4 May 2024.</span></li> <li class="mb-4" id="cite4"><a class="text-blue-500 underline" href="https://ed.stanford.edu/news/what-do-ai-chatbots-really-mean-students-and-cheating">What do AI chatbots really mean for students and cheating?</a> <span>Spector, Carrie. 31 October 2023.</span></li> <li class="mb-4" id="cite5"><a class="text-blue-500 underline" href="https://www.pewresearch.org/short-reads/2023/11/21/what-the-data-says-about-americans-views-of-artificial-intelligence/">GWhat the data says about Americans’ views of artificial intelligence.</a> <span>Faverio, Michelle. Tyson, Alec. 21 November 2023.</span></li> <li class="mb-4" id="cite6"><a class="text-blue-500 underline" href="https://arstechnica.com/information-technology/2023/07/why-ai-detectors-think-the-us-constitution-was-written-by-ai/">Why AI writing detectors don’t work.</a> <span>Edwards, Benj. 14 July 2023.</span></li> <li class="mb-4" id="cite7"><a class="text-blue-500 underline" href="https://www.nytimes.com/2023/02/06/technology/chatgpt-schools-teachers-ai-ethics.html">At This School, Computer Science Class Now Includes Critiquing Chatbots.</a> <span>Singer, Natasha. 6 February 2023.</span></li></ol>',L,f,B='<span class="mx-auto py-4 text-gray-400">Remediation Project for ENGL101 at the University of Maryland</span>';return{c(){s=r("header"),n=r("div"),t=r("h1"),t.textContent=I,b=v(),o=r("div"),i=r("span"),i.innerHTML=W,E=v(),T=r("div"),H=v(),w=r("span"),C=D(p[1]),N=D(" min read"),M=v(),u=r("main"),m=r("div"),m.innerHTML=O,S=v(),g=r("div"),g.innerHTML=R,L=v(),f=r("footer"),f.innerHTML=B,this.h()},l(e){s=l(e,"HEADER",{class:!0});var d=x(s);n=l(d,"DIV",{class:!0});var P=x(n);t=l(P,"H1",{class:!0,"data-svelte-h":!0}),k(t)!=="svelte-1jnpxm5"&&(t.textContent=I),b=A(P),o=l(P,"DIV",{class:!0});var y=x(o);i=l(y,"SPAN",{class:!0,"data-svelte-h":!0}),k(i)!=="svelte-e2mwgk"&&(i.innerHTML=W),E=A(y),T=l(y,"DIV",{class:!0}),x(T).forEach(c),H=A(y),w=l(y,"SPAN",{});var j=x(w);C=F(j,p[1]),N=F(j," min read"),j.forEach(c),y.forEach(c),P.forEach(c),d.forEach(c),M=A(e),u=l(e,"MAIN",{class:!0});var G=x(u);m=l(G,"DIV",{class:!0,"data-svelte-h":!0}),k(m)!=="svelte-ofxgug"&&(m.innerHTML=O),S=A(G),g=l(G,"DIV",{class:!0,"data-svelte-h":!0}),k(g)!=="svelte-14jfxnm"&&(g.innerHTML=R),G.forEach(c),L=A(e),f=l(e,"FOOTER",{class:!0,"data-svelte-h":!0}),k(f)!=="svelte-hkrpla"&&(f.innerHTML=B),this.h()},h(){h(t,"class","text-6xl font-semibold mb-4"),h(i,"class",""),h(T,"class","w-1.5 h-1.5 rounded-full bg-white"),h(o,"class","flex items-center gap-4 text-2xl"),h(n,"class","mt-auto max-w-4xl"),h(s,"class","h-screen flex flex-col bg-blue-500 p-10 text-white"),h(m,"class","flex flex-col w-full mx-auto my-8"),h(g,"class","mb-8"),h(u,"class","w-full"),h(f,"class","w-full flex border-t border-t-gray-300")},m(e,d){_(e,s,d),a(s,n),a(n,t),a(n,b),a(n,o),a(o,i),a(o,E),a(o,T),a(o,H),a(o,w),a(w,C),a(w,N),_(e,M,d),_(e,u,d),a(u,m),p[2](m),a(u,S),a(u,g),_(e,L,d),_(e,f,d)},p(e,[d]){d&2&&Z(C,e[1])},i:z,o:z,d(e){e&&(c(s),c(M),c(u),c(L),c(f)),p[2](null)}}}function X(p,s,n){let t,I=[],b=0;q(()=>{for(const i of t.children)i.tagName=="P"&&I.push(i);n(1,b=K(I))});function o(i){J[i?"unshift":"push"](()=>{t=i,n(0,t)})}return[t,b,o]}class te extends U{constructor(s){super(),Y(this,s,X,Q,V,{})}}export{te as component};
